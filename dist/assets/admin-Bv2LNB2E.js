6f4dc5c60f702dcb851d5279616cfa7ffd093dba
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2025-02-05 17:49:00 +0100

    fix: update Vite configuration to ensure correct path resolution for static assets and compiled files

---
+++ dist/assets/admin-Bv2LNB2E.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";console.log("Admin Page Loaded");
---
+++ dist/assets/main-CcfGIbLy.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";document.addEventListener("DOMContentLoaded",()=>{new e});class e{constructor(){this.render()}render(){document.querySelector("#root")}}
