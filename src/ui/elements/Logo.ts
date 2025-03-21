69a8baaefd551ae386de747e9a1fc8a893eec3c6
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2025-03-21 08:20:11 +0100

    change path to logo

--- src/ui/elements/Logo.ts
+++ src/ui/elements/Logo.ts
@@ -1,6 +1,6 @@
 export const Logo = (): HTMLImageElement => {
     const logo = document.createElement("img");
-    logo.src = "../../../public/assets/logo/logo-dziecko.jpg"; 
+    logo.src = "/assets/logo/logo-dziecko.jpg"; 
     logo.alt = "logo";
     logo.classList.add("header__logo");
 
