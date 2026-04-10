537057612a737575f56a1aa317f1f3a158f08b04
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2026-04-10 04:41:24 +0200

    add backend url

--- src/api/loggedUser.ts
+++ src/api/loggedUser.ts
@@ -1,40 +1,57 @@
-// import { BACK_END_URL } from "../constants/api";
-// import { User } from "../types/User";
-  
-//   export async function loggedUser(user: User): Promise<User[]> {
-//     console.log(user)
-//     const updatedUser = { ...user, lastLogin: new Date().toISOString() }
-//     const response =  await fetch(`${BACK_END_URL}/loggedUser`, {
-//       method: "PUT",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify(updatedUser),
-//     });
-    
-//     const data = await response.json();
-//     const loggedUser: User[] = data; 
-
-//     return loggedUser;
-//   }
-  
-//   export default loggedUser;
-
 import { BACK_END_URL } from "../constants/api";
 import { User } from "../types/User";
-
-export async function loggedUser(user: User): Promise<User> {
-    console.log(user);
-    const updatedUser = { ...user, lastLogin: new Date().toISOString() };
-    
-    const response = await fetch(`${BACK_END_URL}/loggedUser`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(updatedUser),
+  
+  export async function loggedUser(user: User): Promise<User[]> {
+    console.log(user)
+    const updatedUser = { ...user, lastLogin: new Date().toISOString() }
+    const response =  await fetch(`${BACK_END_URL}/loggedUser`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedUser),
     });
-
-    if (!response.ok) throw new Error(`Error: ${response.status}`);
     
-    return updatedUser; 
-}
+    const data = await response.json();
+    const loggedUser: User[] = data; 
 
-export default loggedUser;
+    return loggedUser;
+  }
+  
+  export default loggedUser;
+
+// import { BACK_END_URL } from "../constants/api";
+// import { User } from "../types/User";
+
+// export async function loggedUser(user: User): Promise<User> {
+//     console.log(user);
+//     const updatedUser = { ...user, lastLogin: new Date().toISOString() };
+    
+//     const response = await fetch(`${BACK_END_URL}/loggedUser`, {
+//         method: "PUT",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify(updatedUser),
+//     });
+
+//     if (!response.ok) throw new Error(`Error: ${response.status}`);
+    
+//     return updatedUser; 
+// }
+
+// export default loggedUser;
+
+// import { User } from "../types/User";
+
+// export async function loggedUser(user: User): Promise<User> {
+//   console.log(user);
+
+//   const updatedUser = {
+//     ...user,
+//     lastLogin: new Date().toISOString(),
+//   };
+
+//   localStorage.setItem("loggedUser", JSON.stringify(updatedUser));
+
+//   return updatedUser;
+// }
+
+// export default loggedUser;
   
